5a661efa3c5fe583fea82da9f659c637b186c5c7
Ivan Wills <ivan.wills@gmail.com>
2014-12-07 20:10:09 +1100

    Updating for release of version 0.2.4

--- lib/Getopt/Alt/Exception.pm
+++ lib/Getopt/Alt/Exception.pm
@@ -10,7 +10,7 @@ use Moose;
 use version;
 use overload '""' => sub { shift->message };
 
-our $VERSION = version->new('0.2.3');
+our $VERSION = version->new('0.2.4');
 
 has message => (
     is      => 'rw',
@@ -39,7 +39,7 @@ Getopt::Alt::Exception - I have forgotten where I was going with this
 
 =head1 VERSION
 
-This documentation refers to Getopt::Alt::Exception version 0.2.3.
+This documentation refers to Getopt::Alt::Exception version 0.2.4.
 
 
 =head1 SYNOPSIS
