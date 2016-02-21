f0e7fefe39672cd5f10dd31ddc91df178a4fce06
Ivan Wills <ivan.wills@gmail.com>
2016-02-21 11:47:04 +1100

    Updating for release of version 0.3.7

--- lib/Getopt/Alt.pm
+++ lib/Getopt/Alt.pm
@@ -28,7 +28,7 @@ Moose::Exporter->setup_import_methods(
     as_is => [qw/get_options/],
 );
 
-our $VERSION = version->new('0.3.6');
+our $VERSION = version->new('0.3.7');
 our $EXIT    = 1;
 
 has options => (
@@ -499,7 +499,7 @@ Getopt::Alt - Command line option passing with with lots of features
 
 =head1 VERSION
 
-This documentation refers to Getopt::Alt version 0.3.6.
+This documentation refers to Getopt::Alt version 0.3.7.
 
 =head1 SYNOPSIS
 
--- lib/Getopt/Alt/Exception.pm
+++ lib/Getopt/Alt/Exception.pm
@@ -12,7 +12,7 @@ use overload '""' => sub { shift->message };
 
 extends 'Throwable::Error';
 
-our $VERSION = version->new('0.3.6');
+our $VERSION = version->new('0.3.7');
 
 has help => (
     is  => 'rw',
@@ -37,7 +37,7 @@ Getopt::Alt::Exception - I have forgotten where I was going with this
 
 =head1 VERSION
 
-This documentation refers to Getopt::Alt::Exception version 0.3.6.
+This documentation refers to Getopt::Alt::Exception version 0.3.7.
 
 
 =head1 SYNOPSIS
